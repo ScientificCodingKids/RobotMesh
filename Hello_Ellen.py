e40558fb7ff08e02d5642f201afbba90df46c7ae
ellenzhang108 <50815609+ellenzhang108@users.noreply.github.com>
2019-06-28 21:01:37 -0400

    Add on from video

--- Hello_Ellen.py
+++ Hello_Ellen.py
@@ -11,3 +11,19 @@ print(type(birth_year))
 print(type(age))
 print(age)
 print('Yay you successfully past the test')
+print('Here is an activity you can do')
+command = ''
+while command != 'quit':
+    command = input('> '). lower()
+    if command == 'start':
+        print('Car Started...')
+    elif command == 'Stop':
+        print('Car Stopped')
+    elif command =='help':
+        print('''
+        Start- To Start The Car
+        Stop- To Stop The Car
+        Quit- To Quit
+        ''')
+    else:
+        print('Sorry, what you entered could not be understood')
