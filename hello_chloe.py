0a293fb025ffa6003258c48c9f11428420f71001
f.n <feng.ning@iaqs.org>
2019-12-06 23:59:42 -0500

    clean up files

--- hello_chloe.py
+++
@@ -1,4 +0,0 @@
-print("hello from chloe")
-for i in range(0, 4):
-    print("hello from chloe")
-print("oh hi awesome ellen.")
--- hello_jessica.py
+++
@@ -1,3 +0,0 @@
-print('hello from Jessica!')
-print ('Jessica is clearly Olaf')
-print('i like cookies!!!!!')
